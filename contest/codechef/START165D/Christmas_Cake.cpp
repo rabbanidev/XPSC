182828aaea49040282d4793073238b005650a285
rabbanidev <golamrabbani227417@gmail.com>
2024-12-24 20:17:00 +0600

    Week 9 day 3

---
+++ contest/codechef/START165D/Christmas_Cake.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define el "\n"
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+
+    cout << 25 - x << el;
+
+    return 0;
+}
---
+++ week-9/day-3/A_k_th_divisor.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n, k;
+    cin >> n >> k;
+    vector<long long> divisors;
+    for (long long i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+            if ((n / i) != i)
+            {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+
+    if (divisors.size() < k)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        cout << divisors[k - 1] << '\n';
+    }
+
+    return 0;
+}
